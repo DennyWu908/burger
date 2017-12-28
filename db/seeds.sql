7b5f3695fec32f08bb9bd9d5413567c7b8146175
Denny Wu <denny.wu87@gmail.com>
2017-12-28 02:33:14 -0800

    Created sql files and added code to create burger database with entries

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) values ("The Usual", false);
+
+INSERT INTO burgers (burger_name, devoured) values ("Extra Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured) values ("Has Pickles", false);
