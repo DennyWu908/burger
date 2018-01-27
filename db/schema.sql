1bdd8c2e2bdef2a5feaf938e70a1781806e182f0
Denny Wu <denny.wu87@gmail.com>
2018-01-26 23:44:13 -0800

    Setting up connection js and orm js files

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INTEGER AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN NOT NULL,
+	devoured BOOLEAN NOT NULL DEFAULT false,
 	date TIMESTAMP
+	PRIMARY KEY (id)
 );
